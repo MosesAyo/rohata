9cdacee389982f0dbe9037add257522750369071
Moses Ayo <legendmoses360@yahoo.com>
2022-03-29 10:11:02 +0100

    Fixed a tiny issue

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -50,7 +50,7 @@ export default function Navbar() {
             
             </div>
             {userToken?
-              <div>
+              <div className='hidden md:block'>
                   <p>Hi, {userName}</p>
               </div>:
             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 hidden md:block">
